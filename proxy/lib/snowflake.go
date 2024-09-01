70f3f4e4f120163e79ecda807ba4045f8f00d259
WofWca <wofwca@protonmail.com>
2024-09-01 14:28:53 +0400

    hardening(proxy): check port against relay pattern
    So that the broker cannot tell the proxy to connect to arbitrary ports
    of the Snowflake server machine.
    
    This is a breaking change for proxy users who set
    `allowed-relay-hostname-pattern` explicitly, because
    we renamed that parameter name in this commit.
    For library users this is not breaking.
    
    This does not affect the current production setup of Snowflake.
    
    This is especially useful when `AllowedRelayHostPattern` is lax,
    i.e. when the proxy is able to connect to arbitrary
    (or more or less arbitrary) hosts. See
    [Dedicated Snowflake server port as a way to tell if host allows Snowflake connections](https://gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/snowflake/-/issues/40166)
    
    This commit was originally submitted as part of this MR:
    https://gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/snowflake/-/merge_requests/381.

--- proxy/lib/snowflake.go
+++ proxy/lib/snowflake.go
@@ -144,11 +144,14 @@ type SnowflakeProxy struct {
 	// (see the `Capacity` property).
 	EphemeralMinPort uint16
 	EphemeralMaxPort uint16
-	// RelayDomainNamePattern is the pattern specify allowed domain name for relay
+	// AllowedRelayHostPattern is the pattern specify allowed host
+	// (hostname and, optionally, port) for relay.
 	// If the pattern starts with ^ then an exact match is required.
-	// The rest of pattern is the suffix of domain name.
-	// There is no look ahead assertion when matching domain name suffix,
+	// The rest of pattern is the suffix of host.
+	// There is no look ahead assertion when matching host suffix,
 	// thus the string prepend the suffix does not need to be empty or ends with a dot.
+	AllowedRelayHostPattern string
+	// Deprecated: use AllowedRelayHostPattern instead
 	RelayDomainNamePattern string
 	// AllowProxyingToPrivateAddresses determines whether to allow forwarding
 	// client connections to private IP addresses.
@@ -632,14 +635,14 @@ func (sf *SnowflakeProxy) runSession(sid string) {
 		// Otherwise we'll `tokens.ret()` when the connection finishes.
 	}()
 
-	offer, relayURL := broker.pollOffer(sid, sf.ProxyType, sf.RelayDomainNamePattern)
+	offer, relayURL := broker.pollOffer(sid, sf.ProxyType, sf.AllowedRelayHostPattern)
 	if offer == nil {
 		return
 	}
 	log.Printf("Received Offer From Broker: \n\t%s", strings.ReplaceAll(offer.SDP, "\n", "\n\t"))
 
 	if relayURL != "" {
-		if err := checkIsRelayURLAcceptable(sf.RelayDomainNamePattern, sf.AllowProxyingToPrivateAddresses, sf.AllowNonTLSRelay, relayURL); err != nil {
+		if err := checkIsRelayURLAcceptable(sf.AllowedRelayHostPattern, sf.AllowProxyingToPrivateAddresses, sf.AllowNonTLSRelay, relayURL); err != nil {
 			log.Printf("bad offer from broker: %v", err)
 			return
 		}
@@ -678,7 +681,7 @@ func (sf *SnowflakeProxy) runSession(sid string) {
 
 // Returns nil if the relayURL is acceptable
 func checkIsRelayURLAcceptable(
-	allowedHostNamePattern string,
+	allowedHostPattern string,
 	allowPrivateIPs bool,
 	allowNonTLSRelay bool,
 	relayURL string,
@@ -705,9 +708,11 @@ func checkIsRelayURLAcceptable(
 	if parsedRelayURL.Scheme != "wss" && parsedRelayURL.Scheme != "ws" {
 		return fmt.Errorf("rejected Relay URL protocol: only WebSocket is allowed")
 	}
-	matcher := namematcher.NewNameMatcher(allowedHostNamePattern)
-	if !matcher.IsMember(parsedRelayURL.Hostname()) {
-		return fmt.Errorf("rejected Relay URL: hostname does not match allowed pattern \"%v\"", allowedHostNamePattern)
+	matcher := namematcher.NewNameMatcher(allowedHostPattern)
+	// TODO do not reject client if the port is specified explicitly
+	// in `relayURL` but implicitly in `allowedHostPattern`, and vice versa.
+	if !matcher.IsMember(parsedRelayURL.Host) {
+		return fmt.Errorf("rejected Relay URL: host does not match allowed pattern \"%v\"", allowedHostPattern)
 	}
 	return nil
 }
@@ -761,8 +766,13 @@ func (sf *SnowflakeProxy) Start() error {
 		return fmt.Errorf("invalid default relay url: %s", err)
 	}
 
-	if !namematcher.IsValidRule(sf.RelayDomainNamePattern) {
-		return fmt.Errorf("invalid relay domain name pattern")
+	// RelayDomainNamePattern is deprecated
+	if sf.AllowedRelayHostPattern == "" && sf.RelayDomainNamePattern != "" {
+		sf.AllowedRelayHostPattern = sf.RelayDomainNamePattern
+	}
+
+	if !namematcher.IsValidRule(sf.AllowedRelayHostPattern) {
+		return fmt.Errorf("invalid relay host pattern")
 	}
 
 	if sf.EphemeralMaxPort != 0 {
